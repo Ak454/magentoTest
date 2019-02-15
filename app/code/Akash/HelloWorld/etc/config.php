79da2f99901ba34eb45c90bf996324055339f529
Ak454 <akashparmar1996@gmail.com>
2019-02-15 12:06:38 +0000

    Hello World changes

---
+++ app/code/Akash/HelloWorld/etc/config.php
@@ -0,0 +1,12 @@
+#File: app/etc/config.php
+<?php
+return array (
+  'modules' =>
+  array (
+    'Magento_Store' => 1,
+    'Magento_AdvancedPricingImportExport' => 1,
+    'Magento_Directory' => 1,
+    //...
+    'Akash_HelloWorld'=> 1
+  ),
+);
